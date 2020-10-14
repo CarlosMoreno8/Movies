f6cdb30375aa0d014ba8e5f234abf5e21e96f4f8
Carlos <carlosmorenogarcia8@gmail.com>
2020-10-14 16:33:15 +0200

    remove routes orders and users

--- app.js
+++ app.js
@@ -2,16 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const moviesRouter = require('./routes/movies');
-const ordersRouter = require('./routes/orders');
-const usersRouter = require('./routes/users');
+//const ordersRouter = require('./routes/orders');
+//const usersRouter = require('./routes/users');
 
 app.use(express.json()); //middleware: parsea el body JSON y evita que req.body sea undefined
 
-app.get('/', auth, (req, res) => res.send(req.user))
 
 app.use('/movies', moviesRouter);
-app.use('/orders', ordersRouter);
-app.use('/users', usersRouter);
+//app.use('/orders', ordersRouter);
+//app.use('/users', usersRouter);
 
 
 
