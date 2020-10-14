9e7580a212927ddbe6e4d49c9ba124654845741f
Carlos <carlosmorenogarcia8@gmail.com>
2020-10-14 16:28:20 +0200

    add OrderController and UserController

--- app.js
+++ app.js
@@ -2,17 +2,16 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const moviesRouter = require('./routes/movies');
+const ordersRouter = require('./routes/orders');
+const usersRouter = require('./routes/users');
 
 app.use(express.json()); //middleware: parsea el body JSON y evita que req.body sea undefined
 
+app.get('/', auth, (req, res) => res.send(req.user))
+
 app.use('/movies', moviesRouter);
-
-
-
-
-
-
-
+app.use('/orders', ordersRouter);
+app.use('/users', usersRouter);
 
 
 
